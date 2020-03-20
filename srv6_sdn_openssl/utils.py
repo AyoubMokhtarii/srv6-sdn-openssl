cf22b85e1aa19b18953d4a88687b791d1c7ab8cd
Carmine Scarpitta <carmine.scarpitta.94@gmail.com>
2020-03-20 12:03:05 +0100

    Add get_device_address()

---
+++ srv6_sdn_openssl/utils.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+
+# We use <tenantid>:<deviceid> as device hostname
+def get_device_hostname(tenantid, deviceid):
+    #return '%s:%s' % (tenantid, deviceid)
+    return deviceid
